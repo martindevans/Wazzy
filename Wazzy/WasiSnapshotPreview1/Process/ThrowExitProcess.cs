d70ad7a32f1d95dd4bb2fea77fd16fc31ba0265f
Martin Evans <martindevans@gmail.com>
2023-11-17 16:10:21 +0000

    Added "throw exit process"

---
+++ Wazzy/WasiSnapshotPreview1/Process/ThrowExitProcess.cs
@@ -0,0 +1,21 @@
+﻿using Wasmtime;
+
+namespace Wazzy.WasiSnapshotPreview1.Process;
+
+/// <summary>
+/// Throws a `ThrowExitProcessException` when proc_exit is called
+/// </summary>
+public class ThrowExitProcess
+    : BaseWasiExitProcess
+{
+    protected override void ProcExit(Caller caller, uint code)
+    {
+        throw new ThrowExitProcessException(code);
+    }
+}
+
+public class ThrowExitProcessException(uint exitCode)
+    : Exception
+{
+    public uint ExitCode { get; } = exitCode;
+}
